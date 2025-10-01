f638a2c620ffa82fcbee11cb743ee5a5668d73e1
caz002 <caz002@ucsd.edu>
2025-09-30 21:53:07 -0700

    inital setup

---
+++ components/AuthScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { auth } from "../firebase";
+
+export default function AuthScreen() {
+  return (
+    <View>
+      <Text>This works</Text>
+    </View>
+  );
+}
---
+++ components/CafeDetailsScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function CafeDetailsScreen() {
+  return (
+    <View className="text-4xl text-yellow-300">
+      <Text>This works</Text>
+    </View>
+  );
+}
